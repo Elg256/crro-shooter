aabf82fcf2ce80d0434f7ef7f380596bf82c86e6
elg256 <133916011+Elg256@users.noreply.github.com>
2024-01-18 19:09:46 +0100

    Add files via upload

---
+++ data/tsx/floor and walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="murs" tilewidth="16" tileheight="16" tilecount="160" columns="8">
+ <image source="wallset_16x16 .png" width="128" height="320"/>
+</tileset>
